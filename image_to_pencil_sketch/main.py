d592801a568ea42b73abecec60b9118a7d5c8e25
Madushani Rodrigo <95022003+MadushaniR@users.noreply.github.com>
2023-06-24 23:16:50 +0530

    Update main.py

--- image_to_pencil_sketch/main.py
+++ image_to_pencil_sketch/main.py
@@ -4,7 +4,9 @@ import cv2
 imgLocation = 'D:\Projects\image_to_pencil_sketch\garfield.jpg'
 img = cv2.imread(imgLocation)
 
+# Convert the image to grayscale
 grayImg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
 invertedGrayImage = 255 - grayImg
 blurredImg = cv2.GaussianBlur(invertedGrayImage, (21,21),0)
 invertedBlurredImg = 255 - blurredImg
